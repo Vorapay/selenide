68ad9b18c697e7d8e3767d3e7e476a1e10260f92
Vara <artemiivoropaev@gmail.com>
2025-08-09 01:25:23 +0700

    initial commit

---
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -0,0 +1,37 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+class RegistrationTest {
+
+    public String generateDate(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    void shouldRegisred() {
+        String planingDate = generateDate(4, "dd.MM.yyyy");
+
+        Selenide.open("http://localhost:9999");
+        $(".input__box > [placeholder='Город']").setValue("Томск").press(Keys.TAB);
+        $("[data-test-id='date'] [type='date']").setValue(planingDate);
+        $("[data-test-id='name'] input").setValue("Воропаев Артемий");
+        $("[data-test-id='phone'] input").setValue("+79131038871");
+        $$(".form-field").find(Condition.text("Я соглашаюсь с условиями обработки и использования" +
+                " моих персональных данных")).click();
+        $(".button__icon ~ .button__text").click();
+        // $(".notification__title + .notification__content").find(Condition.text("Встреча успешно забронирована на "))
+        $(Selectors.byText("Встреча успешно забронирована на " + planingDate)).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+}
